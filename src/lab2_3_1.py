9d7b538f15451110423ca579ebb14b39ee5f1eb5
nazartsap21 <nazar.tsap.ua@gmail.com>
2024-03-04 18:22:23 +0200

    fix code

--- src/lab2_3_1.py
+++ src/lab2_3_1.py
@@ -8,15 +8,15 @@ def max_hamsters(s: int, c: int, hamsters: list[list]) -> int:
         else:
             mid = int((high + low) / 2)
         temp_array = sorted([x[0] + x[1] * mid for x in hamsters])
-        if sum(temp_array[:mid]) == s:
+        if sum(temp_array[:mid+1]) == s:
             result = mid + 1
             break
-        elif sum(temp_array[:mid]) > s:
+        elif sum(temp_array[:mid+1]) > s:
             high = mid - 1
-            counted_hamsters[mid] = sum(temp_array[:mid])
-        elif sum(temp_array[:mid]) < s:
+            counted_hamsters[mid] = sum(temp_array[:mid+1])
+        elif sum(temp_array[:mid+1]) < s:
             low = mid + 1
-            counted_hamsters[mid] = sum(temp_array[:mid])
+            counted_hamsters[mid] = sum(temp_array[:mid+1])
             if counted_hamsters[mid + 1] > s:
                 result = mid + 1
                 break
