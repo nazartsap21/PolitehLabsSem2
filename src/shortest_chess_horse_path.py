28bdbcade81262e625032d3685dc59ba22d63fee
nazartsap21 <nazar.tsap.ua@gmail.com>
2024-03-25 23:41:53 +0200

    add exception handler and rework code

--- src/shortest_chess_horse_path.py
+++ src/shortest_chess_horse_path.py
@@ -1,17 +1,3 @@
-import os
-
-
-def get_input(file_path):
-    if os.stat(file_path).st_size == 0:
-        return None, None, None
-    file = open(file_path, 'r')
-    n = int(file.readline())
-    start = tuple(map(int, file.readline().split(' ')))
-    end = tuple(map(int, file.readline().split(' ')))
-    file.close()
-    return start, end, n
-
-
 def write_output(file_path, answer):
     file = open(file_path, 'w')
     file.write(str(answer[0]) + '\n')
@@ -28,9 +14,18 @@ def get_path(from_dict, last_vertex, start_vertex):
     return path[::-1]
 
 
-def shortest_chess_horse_path(start_pos, end_pos, board_s):
-    if start_pos is None:
-        return -1, [None]
+def find_shortest_chess_horse_path(input_file_path, output_file_path):
+    file = open(input_file_path, 'r')
+    try:
+        board_s = int(file.readline())
+        start_pos = tuple(map(int, file.readline().split(' ')))
+        end_pos = tuple(map(int, file.readline().split(' ')))
+    except ValueError:
+        write_output(output_file_path, (-1, [None]))
+        file.close()
+        return
+    file.close()
+
     queue = [start_pos]
     dist_to = {
         start_pos: 0,
@@ -44,7 +39,8 @@ def shortest_chess_horse_path(start_pos, end_pos, board_s):
         vertex = queue.pop(0)
         if vertex not in visited:
             if vertex == end_pos:
-                return dist_to[vertex], get_path(previous_vertex, vertex, start_pos)
+                write_output(output_file_path, (dist_to[vertex], get_path(previous_vertex, vertex, start_pos)))
+                break
             visited.add(vertex)
             for direction in [(2, -1), (2, 1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)]:
                 row, col = vertex[0] + direction[0], vertex[1] + direction[1]
@@ -52,5 +48,4 @@ def shortest_chess_horse_path(start_pos, end_pos, board_s):
                     queue.append((row, col))
                     dist_to[(row, col)] = dist_to[vertex] + 1
                     previous_vertex[(row, col)] = vertex
-
     return None
--- tests/tests_shortest_chess_horse_path.py
+++ tests/tests_shortest_chess_horse_path.py
@@ -4,16 +4,14 @@ from src.shortest_chess_horse_path import *
 
 class TestShortestChessHorsePath(unittest.TestCase):
     def test_given_example(self):
-        start_position, end_position, board_size = get_input("../resources/input.txt")
-        write_output("../resources/output.txt", shortest_chess_horse_path(start_position, end_position, board_size))
+        find_shortest_chess_horse_path("../resources/input.txt", "../resources/output.txt")
         file = open("../resources/output.txt")
         shortest_path = int(file.readline())
         file.close()
         self.assertEqual(shortest_path, 6)
 
     def test_empty_file(self):
-        start_position, end_position, board_size = get_input("../resources/empty_input.txt")
-        write_output("../resources/empty_output.txt", shortest_chess_horse_path(start_position, end_position, board_size))
+        find_shortest_chess_horse_path("../resources/empty_input.txt", "../resources/empty_output.txt")
         file = open("../resources/empty_output.txt")
         shortest_path = int(file.readline())
         file.close()
