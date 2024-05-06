c6ff0bb98efef22e04ebabbbcf4844743ff62a97
nazartsap21 <nazar.tsap.ua@gmail.com>
2024-05-06 23:24:23 +0300

    add lab8 code and tests with resources files

--- src/minimum_cable_length.py
+++ src/minimum_cable_length.py
@@ -1,58 +1,85 @@
-from src.avl_priority_queue import *
+def read_input(input_file_path):
+    """
+    Reads the input file and creates a matrix representing of the graph
+
+    Parameters:
+        input_file_path (str): the path to the input file
+
+    Returns:
+        matrix (list[list]): the adjacency matrix of the graph.
+        Returns 0 if the input_file is empty (a ValueError occurs)
+    """
+    matrix = []
+    file = open(input_file_path, 'r')
+    try:
+        for line in file:
+            neighbours = list(map(int, line.split(', ')))
+            matrix.append(neighbours)
+        file.close()
+
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0 and i != j:
+                    matrix[i][j] = float('inf')
+
+    except ValueError:
+        file.close()
+        return 0
+
+    return matrix
 
 
 def write_output(output_file_path, result):
+    """
+    Writes the result to the output file.
+
+    Parameters:
+        output_file_path (str): the path to the output file
+        result(int): the result to be written to the file
+    """
     file = open(output_file_path, 'w')
     file.write(str(result))
     file.close()
     return
 
 
-def create_adjacency_list(matrix):
-    adjacency_list = [[] for _ in range(len(matrix))]
+def floyd_warshall(matrix):
+    """
+    Implements the Floyd-Warshall algorithm on the given adjacency matrix.
+
+    Parameters:
+        matrix (list[list]): the adjacency matrix of the graph
+
+    Returns:
+        min_cable_length (int): the minimum cable length calculated using Floyd-Warshall algorithm
+    """
+    for k in range(len(matrix)):
+        for i in range(len(matrix)):
+            for j in range(len(matrix)):
+                if matrix[i][j] > matrix[i][k] + matrix[k][j]:
+                    matrix[i][j] = matrix[i][k] + matrix[k][j]
+
+    min_cable_length = 0
     for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            if matrix[i][j] != 0:
-                adjacency_list[i].append((j, matrix[i][j]))
+        min_cable_length += sum(matrix[i])
 
-    return adjacency_list
+    return int(min_cable_length/2)
 
 
-def prims_algorithm(adj_list):
-    visited = set()
-    priority_queue = AVLTree()
-    max_cable_length = 0
-    priority_queue.enqueue(0, 0)
+def find_minimum_cable_length(input_file_path, output_file_path):
+    """
+    Finds the minimum cable length required to connect all islands
 
-    while len(visited) < len(adj_list):
-        current_island = priority_queue.deque()
-        max_cable_length += current_island[1]
-        neighbours = adj_list[current_island[0]]
+    Parameters:
+        input_file_path (str): the path to the input file
+        output_file_path (str): the path to the output file
+    """
+    matrix = read_input(input_file_path)
 
-        for neighbour in neighbours:
-            if neighbour[0] not in visited:
-                priority_queue.enqueue(neighbour[0], neighbour[1])
-        visited.add(current_island[0])
-
-    return max_cable_length
-
-
-def find_minimum_cable_length(input_file, output_file):
-    matrix = []
-    file = open(input_file, 'r')
-    try:
-        for line in file:
-            neighbours = list(map(int, line.split(', ')))
-            matrix.append(neighbours)
-        file.close()
-    except ValueError:
-        write_output(output_file, 0)
-        file.close()
-        return
-
-    adjacency_list = create_adjacency_list(matrix)
-    if [] in adjacency_list:
-        write_output(output_file, -1)
+    if matrix == 0 or matrix == -1:
+        write_output(output_file_path, matrix)
     else:
-        result = prims_algorithm(adjacency_list)
-        write_output(output_file, result)
+        result = floyd_warshall(matrix)
+        write_output(output_file_path, result)
+
+    return
--- tests/tests_minimum_cable_length.py
+++ tests/tests_minimum_cable_length.py
@@ -4,11 +4,19 @@ from src.minimum_cable_length import *
 
 class TestMinimumCableLength(unittest.TestCase):
     def test_case_1(self):
-        find_minimum_cable_length('../resources/islands_input.csv', '../resources/islands_output.csv')
-        file = open('../resources/islands_output.csv', 'r')
+        find_minimum_cable_length('../resources/islands_input_1.csv', '../resources/islands_output_1.csv')
+        file = open('../resources/islands_output_1.csv', 'r')
         result = file.readline()
         file.close()
-        self.assertEqual(result, '21')
+        self.assertEqual(result, '162')
+
+    def test_case_2(self):
+        find_minimum_cable_length('../resources/islands_input_2.csv',
+                                  '../resources/islands_output_2.csv')
+        file = open('../resources/islands_output_2.csv', 'r')
+        result = file.readline()
+        file.close()
+        self.assertEqual(result, '33')
 
     def test_empty_case(self):
         find_minimum_cable_length('../resources/islands_empty_input.csv', '../resources/islands_empty_output.csv')
@@ -16,13 +24,6 @@ class TestMinimumCableLength(unittest.TestCase):
         result = file.readline()
         file.close()
         self.assertEqual(result, '0')
-
-    def test_unconnected_graph_case(self):
-        find_minimum_cable_length('../resources/islands_unconnected_input.csv', '../resources/islands_unconnected_output.csv')
-        file = open('../resources/islands_unconnected_output.csv', 'r')
-        result = file.readline()
-        file.close()
-        self.assertEqual(result, '-1')
 
 
 if __name__ == '__main__':
